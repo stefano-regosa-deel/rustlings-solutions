46ee3affda848455ba69e0bf412e9022f2b6d6e4
Stefano Regosa <regosas@gmail.com>
2020-10-22 01:21:23 +0200

    error_handlign

--- exercises/error_handling/errors1.rs
+++ exercises/error_handling/errors1.rs
@@ -6,37 +6,34 @@
 // this function to have.
 // Execute `rustlings hint errors1` for hints!
 
-// I AM NOT DONE
-
-pub fn generate_nametag_text(name: String) -> Option<String> {
-    if name.len() > 0 {
-        Some(format!("Hi! My name is {}", name))
-    } else {
-        // Empty names aren't allowed.
-        None
-    }
+pub fn generate_nametag_text(name: String) -> Result<String,String>  {
+  if name.len() > 0 {
+    Ok(format!("Hi! My name is {}", name))
+  } else {
+    Err("`name` was empty; it must be nonempty.".into())
+  }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+  use super::generate_nametag_text;
 
-    // This test passes initially if you comment out the 2nd test.
-    // You'll need to update what this test expects when you change
-    // the function under test!
-    #[test]
-    fn generates_nametag_text_for_a_nonempty_name() {
-        assert_eq!(
-            generate_nametag_text("Beyoncé".into()),
-            Some("Hi! My name is Beyoncé".into())
-        );
-    }
+  // This test passes initially if you comment out the 2nd test.
+  // You'll need to update what this test expects when you change
+  // the function under test!
+  #[test]
+  fn generates_nametag_text_for_a_nonempty_name() {
+    assert_eq!(
+      generate_nametag_text("Beyoncé".into()),
+      Ok("Hi! My name is Beyoncé".into())
+    );
+  }
 
-    #[test]
-    fn explains_why_generating_nametag_text_fails() {
-        assert_eq!(
-            generate_nametag_text("".into()),
-            Err("`name` was empty; it must be nonempty.".into())
-        );
-    }
+  #[test]
+  fn explains_why_generating_nametag_text_fails() {
+    assert_eq!(
+      generate_nametag_text("".into()),
+      Err("`name` was empty; it must be nonempty.".into())
+    );
+  }
 }
