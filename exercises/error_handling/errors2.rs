bb2c7c083ad5cb0f4255dd2e1aae0cf0821a2933
Stefano Regosa <regosas@gmail.com>
2020-10-23 16:51:14 +0200

    error 1 ok

--- exercises/error_handling/errors2.rs
+++ exercises/error_handling/errors2.rs
@@ -21,27 +21,27 @@
 use std::num::ParseIntError;
 
 pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
-    let processing_fee = 1;
-    let cost_per_item = 5;
-    let qty = item_quantity.parse::<i32>();
+  let processing_fee = 1;
+  let cost_per_item = 5;
+  let qty = item_quantity.parse::<i32>();
 
-    Ok(qty * cost_per_item + processing_fee)
+  Ok(qty * cost_per_item + processing_fee)
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+  use super::*;
 
-    #[test]
-    fn item_quantity_is_a_valid_number() {
-        assert_eq!(total_cost("34"), Ok(171));
-    }
+  #[test]
+  fn item_quantity_is_a_valid_number() {
+    assert_eq!(total_cost("34"), Ok(171));
+  }
 
-    #[test]
-    fn item_quantity_is_an_invalid_number() {
-        assert_eq!(
-            total_cost("beep boop").unwrap_err().to_string(),
-            "invalid digit found in string"
-        );
-    }
+  #[test]
+  fn item_quantity_is_an_invalid_number() {
+    assert_eq!(
+      total_cost("beep boop").unwrap_err().to_string(),
+      "invalid digit found in string"
+    );
+  }
 }
--- exercises/error_handling/errors3.rs
+++ exercises/error_handling/errors3.rs
@@ -12,7 +12,7 @@ fn main() {
     let mut tokens = 100;
     let pretend_user_input = "8";
 
-    let cost = total_cost(pretend_user_input)?;
+    let cost = total_cost(pretend_user_input);
 
     if cost > tokens {
         println!("You can't afford that many!");
