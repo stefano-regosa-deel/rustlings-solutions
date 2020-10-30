a2254326288d1a10b15f4d99e406f4e718f47718
nrdlab <nrdlab@devmachine.fritz.box>
2020-10-30 01:52:50 +0100

    result1

--- exercises/error_handling/result1.rs
+++ exercises/error_handling/result1.rs
@@ -14,10 +14,15 @@ enum CreationError {
 
 impl PositiveNonzeroInteger {
     fn new(value: i64) -> Result<PositiveNonzeroInteger, CreationError> {
-        Ok(PositiveNonzeroInteger(value as u64))
+        if value < 0 {
+            Err(CreationError::Negative)
+        }else if value == 0 {
+            Err(CreationError::Zero)
+        }else{
+            Ok(PositiveNonzeroInteger(value as u64))
+        }
     }
-}
-
+}  
 #[test]
 fn test_creation() {
     assert!(PositiveNonzeroInteger::new(10).is_ok());
