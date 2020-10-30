c1ad6797ecc52c52001949898eab7e344b61b08c
Stefano Regosa <regosas@gmail.com>
2020-10-30 18:27:37 +0100

    clippy1

--- exercises/clippy/clippy1.rs
+++ exercises/clippy/clippy1.rs
@@ -6,12 +6,12 @@
 // check clippy's suggestions from the output to solve the exercise.
 // Execute `rustlings hint clippy1` for hints :)
 
-// I AM NOT DONE
 
 fn main() {
+    let error_margin = std::f64::EPSILON;
     let x = 1.2331f64;
     let y = 1.2332f64;
-    if y != x {
+    if (x - y).abs() > error_margin {
         println!("Success!");
     }
 }
