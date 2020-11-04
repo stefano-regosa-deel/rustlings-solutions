fc0598d99fbc0868a91652903af4e142bc3f9003
nrdlab <nrdlab@devmachine.fritz.box>
2020-11-04 23:30:19 +0100

    iterators4

--- exercises/standard_library_types/iterators4.rs
+++ exercises/standard_library_types/iterators4.rs
@@ -2,16 +2,8 @@
 
 // I AM NOT DONE
 
-pub fn factorial(num: u64) -> u64 {
-    // Complete this function to return the factorial of num
-    // Do not use:
-    // - return
-    // Try not to use:
-    // - imperative style loops (for, while)
-    // - additional variables
-    // For an extra challenge, don't use:
-    // - recursion
-    // Execute `rustlings hint iterators4` for hints.
+pub fn factorial(num: u64) -> u64 {     
+    (1..=num).product()
 }
 
 #[cfg(test)]
